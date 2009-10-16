6dab87b58a88422836c46fc6ea45b838a195518d
Faraz Masood Khan <faraz@fanaticlab.com>
2009-10-16 11:22:48 +0000

    Uploading Complete Project

---
+++ Implementation/CrystalMapper/CrystalMapper.Test/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OracleClient;
+using System.Data;
+using System.Diagnostics;
+using CoreSystem.Data;
+using CrystalMapper.Generated.BusinessObjects;
+using System.Data.SqlClient;
+
+namespace CrystalMapper.Test
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Simple Queries
+
+            WorkOrder workOrder = new WorkOrder { ProductID = 722 };
+            WorkOrder[] orders = workOrder.ToList();
+            Console.WriteLine("Total order of product Id: {0} are: {1}", workOrder.ProductID, orders.Length);
+            
+            ProductReview productReview = new ProductReview { ReviewerName = "%John%" };
+            ProductReview[] reviews = productReview.ToList();
+            foreach (ProductReview review in reviews)
+            {
+                Console.WriteLine("Product: {0} reviewd by: {1}", review.ProductID, review.ReviewerName);
+            }
+
+            #endregion
+
+            #region Little Complex Queries
+            
+            foreach (WorkOrder workOrder1 in WorkOrder.OrdersWithQtyGreaterThen(8))
+            {
+                Console.WriteLine("Order ID: {0}, Due Date: {1:f}", workOrder1.WorkOrderID, workOrder1.DueDate); 
+            }
+
+            #endregion
+
+            UpdateProductReviews();
+
+            Console.ReadLine();
+        }
+        
+        #region Bulk Update Example
+
+        public static void UpdateProductReviews()
+        {
+            ProductReview productReview = new ProductReview { ReviewerName = "%John%" };
+            ProductReview[] reviews = productReview.ToList();
+            foreach (ProductReview review in reviews)
+            {
+                review.ReviewerName = "Faraz";
+            }
+
+            ProductReview.Update(reviews); // Updates all reviews in a transaction
+            // Or Simple call review.Update() for individual object update
+            // Or Use review.Update(DataContext) function for update in transaction
+        }
+
+        #endregion
+
+        #region Performance test
+
+        public static void DoTest()
+        {
+            DataTableTest(WorkOrder.TABLE_NAME);
+            CrystalMapperTest<WorkOrder>();
+        }
+
+        public static T[] CrystalMapperTest<T>() where T : Entity<T>, new()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            T searchT = new T();
+            T[] searchTToList = searchT.ToList();
+            stopWatch.Stop();
+            Console.WriteLine("Number of records fetch: {0} by Crystal Mapper of in Time Seconds: {1} ", searchTToList.Length, stopWatch.Elapsed.TotalSeconds);
+            return searchTToList;
+        }
+
+        public static DataTable DataTableTest(string tableName)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            using (SqlConnection conn = new SqlConnection(@"Data Source=DEV-MAC\SQLEXPRESS;Initial Catalog=AdventureWorks;Integrated Security=True"))
+            {
+                using (SqlCommand com = new SqlCommand("SELECT * FROM " + tableName, conn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(com);
+                    DataSet dataset = new DataSet();
+                    adapter.Fill(dataset, tableName);
+                    stopWatch.Stop();
+                    Console.WriteLine("Number of records fetch: {0} through DataSet in Time Seconds: {1} ", dataset.Tables[tableName].Rows.Count, stopWatch.Elapsed.TotalSeconds);
+                    return dataset.Tables[tableName];
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Implementation/CrystalMapper/CrystalMapper/Data/DataContext.cs
@@ -0,0 +1,112 @@
+﻿/*
+ * Author: Faraz Masood Khan 
+ * 
+ * Date: 6/5/2009 2:09:52 PM
+ * 
+ * Class: TableAttribute
+ * 
+ * Copyright: Faraz Masood Khan @ Copyright ©  2009
+ * 
+ * Email: mk.faraz@gmail.com
+ * 
+ * Blogs: http://csharplive.wordpress.com, http://farazmasoodkhan.wordpress.com
+ * 
+ */
+
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+
+using CoreSystem.Data;
+
+namespace CrystalMapper.Data
+{
+    public class DataContext : IDisposable
+    {
+        private bool disposeConnection = false;
+        private Database database;
+        private DbConnection connection;
+        private DbTransaction transaction;   
+
+        public DataContext(Database database)
+            : this(database, database.CreateConnection())
+        {
+            disposeConnection = true;
+        }
+
+        public DataContext(Database database, DbConnection connection)
+        {
+            this.database = database;
+            this.connection = connection;
+        }
+
+        public DbConnection Connection 
+        {
+            get { return this.connection; }
+        }
+        
+        public DbTransaction Transaction 
+        {
+            get { return this.transaction; }
+        }
+
+        public void BeginTransaction()
+        {
+            this.transaction = this.connection.BeginTransaction();
+        }
+
+        public void BeginTransaction(IsolationLevel isolationLevel)
+        {
+            this.transaction = this.connection.BeginTransaction(isolationLevel);
+        }
+
+        public DbCommand CreateCommand(string cmdText)
+        {
+            return (this.transaction != null ?
+                this.database.CreateCommand(cmdText, transaction) : this.database.CreateCommand(cmdText, this.connection));
+        }
+
+        public DbParameter CreateParameter()
+        {
+            return this.database.CreateParameter();
+        }
+
+        public DbParameter CreateParameter(object value, string paramName)
+        {
+            return this.database.CreateParameter(value, paramName);
+        }
+
+        public string ToString(object value)
+        {
+            return this.database.ToString(value);
+        }      
+
+        public void CommitTransaction()
+        {
+            if (this.transaction != null)
+                transaction.Commit();   
+        }    
+
+        public void RollbackTransaction()
+        {
+            if (this.transaction != null)
+                transaction.Rollback();
+            this.transaction = null;
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (this.transaction != null)
+                this.transaction.Dispose();
+
+            if (this.disposeConnection)
+                this.connection.Dispose();
+              
+        }
+
+        #endregion
+    }
+}
